40c809342321d62230b241b618d48ca1735de832
Loick Baude <loick.baude@ig2i.centralelille.fr>
2021-05-17 20:07:35 +0200

    update to Android Q

--- app/src/main/java/com/example/chat2021/LoginActivity.java
+++ app/src/main/java/com/example/chat2021/LoginActivity.java
@@ -6,7 +6,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -15,7 +15,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
