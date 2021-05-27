41dc103ba40b33299577423e8cb14b5c1dfad841
Nathan Capiaux <nathan.capiaux@ig2i.centralelille.fr>
2021-05-27 17:48:40 +0200

    refactor login menu

--- app/src/main/java/com/example/chat2021/LoginActivity.java
+++ app/src/main/java/com/example/chat2021/LoginActivity.java
@@ -2,8 +2,6 @@ package com.example.chat2021;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,26 +12,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -121,15 +104,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login);
 
         sp = PreferenceManager.getDefaultSharedPreferences(this);
         editor = sp.edit();
 
-        edtLogin = findViewById(R.id.login_edtLogin2);
-        edtPasse = findViewById(R.id.login_edtPasse2);
-        cbRemember = findViewById(R.id.login_cbRemember2);
-        btnOK = findViewById(R.id.login_btnOK2);
+        edtLogin = findViewById(R.id.login_edtLogin);
+        edtPasse = findViewById(R.id.login_edtPasse);
+        cbRemember = findViewById(R.id.login_cbRemember);
+        btnOK = findViewById(R.id.login_btnOK);
 
         btnOK.setOnClickListener(this);
     }
